cd268d877741fd41055c71afa88d710fecf948dd
Xenetics <xenetics@live.com>
2014-07-03 01:32:50 -0400

    Working on ads

--- Tiltiland/src/scom/game/tiltiland/Ad.java
+++ Tiltiland/src/scom/game/tiltiland/Ad.java
@@ -1,20 +1,55 @@
 package scom.game.tiltiland;
 
+import com.game.tiltiland.R;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
-import com.game.tiltiland.R;
-import com.google.android.gms.ads.*;
+/**
+ * Example of including a Google banner ad in code and listening for ad events.
+ */
+public class Ad extends Activity 
+{
+    private AdView mAdView;
 
-public class Ad extends Activity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_tiltiland);
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tiltiland);
 
-    // Look up the AdView as a resource and load a request.
-    AdView adView = (AdView)this.findViewById(R.id.adView);
-    AdRequest adRequest = new AdRequest.Builder().build();
-    adView.loadAd(adRequest);
-  }
+        mAdView = new AdView(this);
+        mAdView.setAdUnitId(getResources().getString(R.string.ad_unit_id));
+        mAdView.setAdSize(AdSize.BANNER);
+        mAdView.setAdListener(new ToastAdListener(this));
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.adView);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        layout.addView(mAdView, params);
+        mAdView.loadAd(new AdRequest.Builder().build());
+    }
+
+    @Override
+    protected void onPause() 
+    {
+        mAdView.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() 
+    {
+        super.onResume();
+        mAdView.resume();
+    }
+
+    @Override
+    protected void onDestroy() 
+    {
+        mAdView.destroy();
+        super.onDestroy();
+    }
 }
